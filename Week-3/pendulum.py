de1b37f6116d635930e47dc06b93ab54716a418a
Tiffany <qingchen.qiao19@imperial.ac.uk>
2019-11-11 14:32:55 +0000

    add

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,3 +1,20 @@
-def period()
+import math
+def period():
+    while True:
+        try:
+            argu1=int(input("L:"))
+            argu2=int(input("g:"))
 
-''''''
+   
+        
+        except TypeError:
+            print("Sorry, I didn't understand that.")
+            continue
+        except ValueError:
+            print("Sorry,the equation cannot correctly compute")
+            continue
+        else:
+            break
+    
+    return 2*math.pi*(argu1/argu2)**(1/2)
+print(period())
